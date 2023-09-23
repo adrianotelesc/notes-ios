139f7e9a2775bad6121d1af6e006adbb972bc15c
Adriano Teles <adriano.telesc@gmail.com>
2023-09-23 11:52:32 -0400

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Adriano Teles on 23/09/23.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
